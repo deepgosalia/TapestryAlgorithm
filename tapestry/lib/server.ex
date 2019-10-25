3b8bce40746bb5649264afaf86223e751e188194
deepgosalia <deepgosalia30@gmail.com>
2019-10-25 03:15:32 -0400

    Added Insert Function

--- tapestry/lib/server.ex
+++ tapestry/lib/server.ex
@@ -3,6 +3,8 @@ defmodule Server do
   def start_link(state) do
     {:ok,pid} = GenServer.start_link(__MODULE__,state) # [hashID,]
     hashID = Enum.at(state,0)
+    IO.inspect(pid)
+   # IO.inspect(Enum.at(state, 1))
     :ets.insert(:processTable,{hashID,pid})
   end
 
@@ -11,6 +13,10 @@ defmodule Server do
     {:ok,state}
   end
 
+
+
+  #00000000000000000000000000000000000000000000000000000000000000000
+  #below are the get and set functions--------------------------------
   def get_state(pid) do
     state=GenServer.call(pid,:get_state)
     state
@@ -21,6 +27,7 @@ defmodule Server do
   end
 
   def getProcessId(hashID) do
+    #IO.puts(hashID)
     [{_,pid}] = :ets.lookup(:processTable,hashID)
     pid
   end
@@ -31,59 +38,127 @@ defmodule Server do
     levelList
   end
 
+  def handle_cast({:updateList,list},state) do
+    [id,_list] = state
+    {:noreply,[id,list]}
+  end
+  #---------------------------------------------------------
 
+# to generate filled table------------------------
+
+def generatetable() do
+
+end
+
+
+#------------------------------------
+
+  #to insert a single node with a given root-------------------
+  #findRoot(root,insert_node)
 
   def insertnode(root,insert_node) do
+
    root_id = getProcessId(root)
    level = findMaxPrefixMatch(root, insert_node)
-   root_list = getListAt(root_id,level)
-   stringArray = String.codepoints(insert_node)
+    root_list = getListAt(root_id,level)
+    stringArray = String.codepoints(insert_node)
    char_val = Enum.at(stringArray,level)
-   char_pos = Integer.parse(char_val,16)
+   {char_pos,_} = Integer.parse(char_val,16)
+
    field = Enum.at(root_list,char_pos)
 
-   updateNewNodeTable(insert_node,root_id,level)
+   updateNewNodeTable(insert_node,root,level)
 
-   if(field==0) do
+   if(field==nil) do
      #add it there
      updateRootTable(insert_node,level,char_pos,root_id)
    else
     node=findNodeWithMinDist(root,insert_node)
     #update value in its table
    end
+
+   #ackmulticast
+   #get updated root value
+   #for each using enum.each send multicast to that node
+   #there get id for that node and insertnode() without modifying the new node(so add flag)
+   # go till last level
   end
 
-  def updateNewNodeTable(new_node,root_node_id,uptoLevel) do
-    pid = getProcessId(new_node)
-    GenServer.cast(pid,{:update_node_table,new_node,root_node_id,uptoLevel})
+
+  def test_node(hashID) do
+    pid = getProcessId(hashID)
+    state=get_state(pid)
+    IO.inspect(Enum.at(state,1))
   end
 
+  #Ack Multicast----------------------------------------------
 
-  def handle_cast({:updateAtLevel,list_insert,x},_state) do
-    state=get_state(self())
+  def ackMulticast(level) do
+
+  end
+
+  #----------------------------------------------------
+
+  #here below we copy from root to node level------------------
+
+
+
+
+def updateNewNodeTable(new_node,root_node_id,uptoLevel) do
+  pid = getProcessId(new_node)
+  root_id = getProcessId(root_node_id)
+  state = get_state(root_id)
+  root_list = Enum.at(state,1)
+  state=get_state(pid)
+  node_list = Enum.at(state,1)
+
+  Enum.each(0..uptoLevel,fn(x)->
+    temp_list = Enum.at(root_list,x)
+    new_list=List.replace_at(node_list,x,temp_list)
+    GenServer.cast(pid,{:updateList,new_list})
+  end)
+
+
+  #GenServer.cast(pid,{:update_node_table,new_node,root_node_id,uptoLevel})
+end
+
+  #---------------------------------------------------
+   # to update single value at a position
+
+   def updateRootTable(node_value,level,col,pid) do
+    GenServer.cast(pid,{:update_node,node_value,level,col,pid})
+  end
+
+  def handle_cast({:update_node,node_value,level,col,pid},state) do
     node_list = Enum.at(state,1)
-    new_list=List.replace_at(node_list,x,list_insert)
-    {:noreply,[Enum.at(state,0),new_list]}
+    new_list=List.replace_at(Enum.at(node_list,level),col,node_value)
+    new_temp =List.replace_at(node_list,level,new_list)
+    {:noreply,[Enum.at(state,0),new_temp]}
   end
 
-  def handle_cast({:update_node_table,_new_node,root_node_id,level},state) do
-    root_list = Enum.at(get_state(root_node_id),1)
-    Enum.each(0..level,fn(x)->
-      temp_list = Enum.at(root_list,x)
-      GenServer.cast(self(),{:updateAtLevel,temp_list,x})
-    end)
-    {:noreply,state}
-  end
+  #----------------------------------------------------
 
-  def updateRootTable(node_value,level,col,pid) do
-    GenServer.cast(pid,{:update_node,node_value,level,col})
-  end
+  # def handle_cast({:update_node_table,_new_node,root_node_id,level},state) do
+  #  # IO.inspect(root_id)
+  #   root_id = getProcessId(root_node_id)
+  #   root_list = Enum.at(get_state(root_id),1)
+  #   Enum.each(0..level,fn(x)->
+  #     temp_list = Enum.at(root_list,x)
+  #     GenServer.cast(self(),{:updateAtLevel,temp_list,x})
+  #   end)
+  #   {:noreply,state}
+  # end
 
-  def handle_cast({:update_node,node_value,level,col},state) do
-    node_list = Enum.at(get_state(self()),1)
-    List.replace_at(Enum.at(node_list,level),
-  end
+  # def handle_cast({:updateAtLevel,list_insert,x},_state) do
+  #   state=get_state(self())
+  #   node_list = Enum.at(state,1)
+  #   new_list=List.replace_at(node_list,x,list_insert)
+  #   {:noreply,[Enum.at(state,0),new_list]}
+  # end
 
+#---------------------------------------
+
+#to find which one has minimum distance
   def findNodeWithMinDist(new_field,curr_field) do
     a = Integer.parse(new_field,16)
     b = Integer.parse(curr_field,16)
@@ -94,7 +169,7 @@ defmodule Server do
     end
   end
 
-
+#to find to what extend are two nodes share common prefix
   def findMaxPrefixMatch(a,b) do
     a=Enum.reduce_while(0..7,0,fn i,acc->
       x = String.slice(a,i,1)
--- tapestry/lib/tapestry.ex
+++ tapestry/lib/tapestry.ex
@@ -1,35 +1,106 @@
 defmodule Tapestry do
   def start() do
-    [numNodes, numRequest] = System.argv()
-    numNodes = String.to_integer(numNodes)
-    numRequest = String.to_integer(numRequest)
+    [numNodes, _numRequest] = System.argv()
+    numNodes = String.to_integer(numNodes)-1   # we are creating first 99 nodes and then other 1 node
+    #_numRequest = String.to_integer(numRequest)
 
     :ets.new(:processTable,[:set,:public,:named_table])
+    :ets.new(:network,[:set,:public,:named_table])
     #ets table to store list of process and its hashID {key,value}->{hashID,pid}
 
-    Enum.each(1..numNodes,fn(x)->
+    temp=Enum.reduce(1..numNodes,[],fn(x,hashList)->
       hashID = :crypto.hash(:sha,Integer.to_string(x))|>Base.encode16 |>String.slice(0..7)
-      list = generateList(x)
-      #list = insertNeighbour(x,list)
-      Server.start_link([hashID,list])
+      hashList++[hashID]
     end)
 
-    Server.isnertNode()
+    Enum.each(1..numNodes, fn(x)->
+      hashID = :crypto.hash(:sha,Integer.to_string(x))|>Base.encode16 |>String.slice(0..7)
+      #spawn fn-> genList(temp,numNodes,x) end
+
+      genList(temp,numNodes,x)
+    end)
+    new_root=findRoot(temp,"B1D57811",[],0,0)
+    list = generateList(10)
+    #IO.inspect(list)
+    Server.start_link(["B1D57811",list])
+    # pid = Server.getProcessId("B1D57811")
+    # state = Server.get_state(pid)
+    # IO.inspect(state)
+    Server.insertnode(new_root,"B1D57811")
+    Server.test_node("B1D57811")
+    Server.test_node(new_root)
+
+    #IO.puts(new_root)
+    loop()
+  end
+
+  def loop() do
+    loop()
   end
   def generateList(x) do
   #8 levels(row) and 16 (hex) cols
     codeString=:crypto.hash(:sha,Integer.to_string(x))|>Base.encode16 |>String.slice(0..7) # BDF23E
     stringArray = String.codepoints(codeString) # B D F 2 3 E
-  list = Enum.reduce(0..7,[],fn(rowNo,temp)  ->
-          tempList =List.duplicate(0,16)
+  _list = Enum.reduce(0..7,[],fn(rowNo,temp)  ->
+          tempList =List.duplicate(nil,16)
           {t,_}=Integer.parse(Enum.at(stringArray,rowNo),16)
           tempList = List.replace_at(tempList,t,codeString)
-          #IO.inspect(tempList)
-          temp = temp++[tempList]
+          _temp = temp++[tempList]
   end)
- # IO.inspect(list)
  end
 
+
+ def genList(t,numNodes,i) do   #356A192B
+  codeString=:crypto.hash(:sha,Integer.to_string(i))|>Base.encode16 |>String.slice(0..7) # BDF23E
+  hashID=Enum.filter(t,fn x-> x != codeString end)
+
+  list = Enum.reduce(0..7,[],fn row,temp ->
+    difList=Enum.filter(hashID,fn x-> String.slice(codeString,0,row)==String.slice(x,0,row) and String.slice(codeString,0,row+1)!=String.slice(x,0,row+1) end)
+    final=Enum.reduce(0..15,[],fn col,some ->
+          coList=Enum.filter(difList,fn dif-> String.slice(dif,row,1) == Integer.to_string(col, 16) end)
+          if length(coList)<=1 do
+            put_list=List.first(coList)
+            some++[put_list]
+          else
+            put_list=findRoot(coList,codeString,[],0,0)
+            some++[put_list]
+          end
+    end)
+
+    stringArray = String.codepoints(codeString) # B D F 2 3 E
+    {t,_}=Integer.parse(Enum.at(stringArray,row),16)
+    final = List.replace_at(final,t,codeString)
+
+    temp= temp ++ [final]
+  end)
+  Server.start_link([codeString,list])
+end
+
+
+ def findRoot(network_list,new_node,neigh_list,pos,added_weight) do  #new node = "86C8BF23"
+ char_node = String.at(new_node,pos)
+ {t,_} = Integer.parse(char_node, 16)
+  char_node=Integer.to_string(rem(t+added_weight,16),16)
+  updated_neigh=Enum.reduce(network_list,[],fn(element,temp)->
+    char_root = String.at(element,pos)
+    if(char_node==char_root) do
+       _temp=temp++[element]
+    else
+      _temp = temp++[]
+      end
+  end)
+  if updated_neigh==[] do
+    findRoot(network_list,new_node,neigh_list,pos,added_weight+1)
+  else
+    if length(updated_neigh)==1 do
+     Enum.at(updated_neigh,0)
+    else
+      if(pos<String.length(new_node)-1) do
+        findRoot(updated_neigh,new_node,neigh_list,pos+1,0)
+      end
+  end
+  end
+ end
 
 end
 Tapestry.start
